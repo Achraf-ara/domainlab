ffd5d099615b81bce1ee5a243c0fefd4ab347f6e
Achraf-ara <achraf.mouljebouj98@gmail.com>
2025-08-10 02:43:09 +0100

    Changed Package Json

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React from "react";
+import type React from "react"
 import type { ReactElement } from "react"
 import { useRouter } from "next/navigation"
 import { useState } from "react"
